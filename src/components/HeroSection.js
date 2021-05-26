50220511e8b33270c271b8ee1736057b3559b754
arjun-1510 <Arjung1510@gmail.com>
2021-05-26 10:59:36 +0530

    initial push

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted />
+      
       <h1>Campus2Cognizant</h1>
       <p><center>Building a stronger you, by bringing techonoly closer to you</center></p>
       <div className='hero-btns'>
